208ff7f0cc14a370bca3e93ffcd7b8e681c61b7d
aaokweb <aaokweb@gmail.com>
2023-03-17 14:50:07 +0530

    added cors

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.use(cors());
 
 app.set('trust proxy', 1);
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
+
 app.use(
 	session({
 		secret: 'secret',
